6b39f02d3af61317256a47103ce43e9802733853
N0m1z <nomis13021@outlook.de>
2021-04-09 16:57:22 +0200

    Comment out picture properties

--- AppetiteAPI/AppetiteAPI/Models/Product.cs
+++ AppetiteAPI/AppetiteAPI/Models/Product.cs
@@ -9,6 +9,6 @@ namespace AppetiteAPI.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public List<string> Ingredients { get; set; }
-        public List<Pic> Pictures { get; set; }
+        //public List<Pic> Pictures { get; set; }
     }
 }
--- AppetiteAPI/AppetiteAPI/Models/Review.cs
+++ AppetiteAPI/AppetiteAPI/Models/Review.cs
@@ -10,6 +10,6 @@ namespace AppetiteAPI.Models
         public DateTime CreationTime { get; set; }
         public string Text { get; set; }
         public Rating Rating { get; set; }
-        public List<Pic> Pictures { get; set; }
+        //public List<Pic> Pictures { get; set; }
     }
 }
