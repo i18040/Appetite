1f2f7dfa9ae529a590c493a55285ce05c2727f89
Marius Dziuba <marius.dziuba@mrs-electronic.de>
2021-04-13 18:27:36 +0200

    delete double class

--- AppetiteAPI/AppetiteAPI/Controllers/RestaurantAdministrationController.cs
+++ AppetiteAPI/AppetiteAPI/Controllers/RestaurantAdministrationController.cs
@@ -1,64 +1,61 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AppetiteAPI.ApiModels;
+using AppetiteAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using AppetiteAPI.Services;
-using AppetiteAPI.ApiModels;
 
 namespace AppetiteAPI.Controllers
 {
-    public class RestaurantAdministrationController
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantAdministrationController : ControllerBase
     {
-        [ApiController]
-        [Route("[controller]")]
-        public class UserServiceController: ControllerBase
+        private IRestaurantAdministrationService _restaurantAdministrationService;
+
+        public RestaurantAdministrationController( IRestaurantAdministrationService restaurantAdministrationService )
         {
-            private IRestaurantAdministrationService _restaurantAdministrationService;
+            _restaurantAdministrationService = restaurantAdministrationService;
+        }
 
-            public UserServiceController( IRestaurantAdministrationService restaurantAdministrationService )
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate( [FromBody] AuthenticateModel model )
+        {
+            var response = _restaurantAdministrationService.Authenticate(model.Email, model.Password);
+
+            if( response == null )
+                return BadRequest(new { message = "E-Mail or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Create( [FromBody] CreateRestaurantModel model )
+        {
+            if( _restaurantAdministrationService.IsEmailRegisteredAlready(model.Email) )
             {
-                _restaurantAdministrationService = restaurantAdministrationService;
+                return Conflict(new { message = "Email is already taken" });
             }
 
-            [AllowAnonymous]
-            [HttpPost("authenticate")]
-            public async Task<IActionResult> Authenticate( [FromBody] AuthenticateModel model )
+            _restaurantAdministrationService.CreateRestaurant(model.Name, model.Password, model.Adress, model.Email, model.PhoneNumber, model.RestaurantType);
+
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpDelete]
+        public async Task<IActionResult> Delete( [FromBody] DeleteRestaurantModel model )
+        {
+            var tokenEmail = User.Identity.Name;
+            if( model.Email != tokenEmail )
             {
-                var response = _restaurantAdministrationService.Authenticate(model.Email, model.Password);
-
-                if( response == null )
-                    return BadRequest(new { message = "E-Mail or password is incorrect" });
-
-                return Ok(response);
+                return new UnauthorizedResult();
             }
 
-            [AllowAnonymous]
-            [HttpPost]
-            public async Task<IActionResult> Create( [FromBody] CreateRestaurantModel model )
-            {
-                if( _restaurantAdministrationService.IsEmailRegisteredAlready(model.Email) )
-                {
-                    return Conflict(new { message = "Email is already taken" });
-                }
+            _restaurantAdministrationService.DeleteRestaurant(model.Email);
 
-                _restaurantAdministrationService.CreateRestaurant(model.Name, model.Password, model.Adress, model.Email, model.PhoneNumber, model.RestaurantType);
-
-                return Ok();
-            }
-
-            [Authorize]
-            [HttpDelete]
-            public async Task<IActionResult> Delete( [FromBody] DeleteRestaurantModel model )
-            {
-                var tokenEmail = User.Identity.Name;
-                if( model.Email != tokenEmail )
-                {
-                    return new UnauthorizedResult();
-                }
-
-                _restaurantAdministrationService.DeleteRestaurant(model.Email);
-
-                return Ok();
-            }
+            return Ok();
         }
     }
 }
