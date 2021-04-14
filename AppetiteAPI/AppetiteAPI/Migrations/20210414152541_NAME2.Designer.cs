634e044131da515b3ef894ad2a41f49e1ca52cc7
Marius Dziuba <marius.dziuba@mrs-electronic.de>
2021-04-14 17:29:55 +0200

    new db migration

--- AppetiteAPI/AppetiteAPI/Migrations/20210414152541_NAME2.Designer.cs
+++ AppetiteAPI/AppetiteAPI/Migrations/20210414152541_NAME2.Designer.cs
@@ -4,14 +4,16 @@ using AppetiteAPI.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppetiteAPI.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210414152541_NAME2")]
+    partial class NAME2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
