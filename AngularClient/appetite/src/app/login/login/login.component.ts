482891064a174d70d3d297dca3633466f59a7ae5
Stephan-Uhl <stephan.uhl@hansgrohe.com>
2021-04-22 19:18:24 +0200

    create authService

--- AngularClient/appetite/src/app/login/login/login.component.ts
+++ AngularClient/appetite/src/app/login/login/login.component.ts
@@ -1,21 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, Validator, Validators } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { AuthServiceService } from 'src/app/service/auth-service.service';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent implements OnInit {
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.scss']
+// })
 
-  constructor(
-    private router: Router
-  ) { }
+// export class LoginComponent implements OnInit {
+// 	constructor(
+// 		private fb: FormBuilder,
+// 		private authService: AuthServiceService
+// 		private router: Router
+// 	) {}
 
-  ngOnInit(): void {
-  }
+// 	error;
 
-  login():void {
-    this.router.navigate(['']);
-  }
-}
+// 	form = this.fb.group({
+// 		email: ['', [Validators.required, Validators.email]],
+// 		password: ['', Validators.required],
+// 	});
+
+// 	isLoading = false;
+
+
+// 	ngOnInit() {
+// 		this.form.valueChanges.subscribe(() => {
+// 			this.error = undefined;
+// 		});
+// 	}
+
+// 	async login() {
+// 		this.error = undefined;
+// 		this.isLoading = true;
+// 		try {
+// 			await this.authService.login(
+// 				this.form.get('email').value,
+// 				this.form.get('password').value
+// 			);
+// 			this.router.navigate(['']);
+// 		} catch (err) {
+// 			console.error('Error while logging in', err);
+// 			this.error = err;
+// 		} finally {
+// 			this.isLoading = false;
+// 		}
+// 	}
+// }
