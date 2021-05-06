8e98be48ff2eca59336474445ece01550ad87684
Tobias Goetz <tobias_goetz@yahoo.de>
2021-05-06 16:21:57 +0200

    Change register site layout. Data sent to backend server still has to be changed.

--- AngularClient/appetite/src/app/login/register/register.component.ts
+++ AngularClient/appetite/src/app/login/register/register.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { IRegistrationRequest } from 'src/app/model/auth/AuthDTO';
+import { PasswordErrorStateMatcher } from '../_helper/PasswordErrorStateMatcher';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: 'mh-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+	constructor(
+		private fb: FormBuilder,
+		private authService: AuthService,
+		private router: Router
+	) {}
 
-  constructor() { }
+	isError = false;
 
-  ngOnInit(): void {
-  }
+	matcher = new PasswordErrorStateMatcher();
 
+	createForm = this.fb.group(
+		{
+			firstname: ['', Validators.required],
+			lastname: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', Validators.required],
+			passwordRepeat: [''],
+		},
+		{ validator: this.checkPasswords }
+	);
+
+	async register() {
+		const user: IRegistrationRequest = {
+			firstname: this.createForm.get('firstname').value,
+			lastname: this.createForm.get('lastname').value,
+			email: this.createForm.get('email').value,
+			password: this.createForm.get('password').value,
+		};
+
+		try {
+			const registeredUser = await this.authService.register(user);
+			this.router.navigate(['../login']);
+		} catch (err) {
+			console.error('Error while sending registration request', err);
+			this.isError = true;
+		}
+	}
+
+	checkPasswords(group: FormGroup) {
+		const pass = group.get('password').value;
+		const confirmPass = group.get('passwordRepeat').value;
+
+		return pass === confirmPass ? null : { notSame: true };
+	}
 }
