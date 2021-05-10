89a73e77b581366a554769d15e756ef96f305832
N0m1z <nomis13021@outlook.de>
2021-05-10 22:55:20 +0200

    Add migrations for previous commit

---
+++ AppetiteAPI/AppetiteAPI/Migrations/20210507234529_FixOrders.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppetiteAPI.Migrations
+{
+    public partial class FixOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_OrderProductsSet_OrderProductsId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderProductsId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderProductsId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "OrderProductsSet",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProductsSet_ProductId",
+                table: "OrderProductsSet",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProductsSet_Products_ProductId",
+                table: "OrderProductsSet",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProductsSet_Products_ProductId",
+                table: "OrderProductsSet");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderProductsSet_ProductId",
+                table: "OrderProductsSet");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "OrderProductsSet");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderProductsId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderProductsId",
+                table: "Products",
+                column: "OrderProductsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_OrderProductsSet_OrderProductsId",
+                table: "Products",
+                column: "OrderProductsId",
+                principalTable: "OrderProductsSet",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ AppetiteAPI/AppetiteAPI/Migrations/20210510203027_FixPropName.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppetiteAPI.Migrations
+{
+    public partial class FixPropName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Products_TypeId",
+                table: "OrderProduct");
+
+            migrationBuilder.RenameColumn(
+                name: "TypeId",
+                table: "OrderProduct",
+                newName: "ProductsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_TypeId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_ProductsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Products_ProductsId",
+                table: "OrderProduct",
+                column: "ProductsId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Products_ProductsId",
+                table: "OrderProduct");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductsId",
+                table: "OrderProduct",
+                newName: "TypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_ProductsId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Products_TypeId",
+                table: "OrderProduct",
+                column: "TypeId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
