b924a2582362dac3a899d9144a8a3c89a2b57e61
Stephan-Uhl <stephan.uhl@hansgrohe.com>
2021-05-18 18:17:33 +0200

    change starting Page to login and add home path

--- AngularClient/appetite/src/app/app-routing.module.ts
+++ AngularClient/appetite/src/app/app-routing.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () =>
-			import('./home/home.module').then(m => m.HomeModule),
-	},
-	{
-		path: 'login',
-		loadChildren: () =>
-			import('./login/login.module').then(m => m.LoginModule),
-	},
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadChildren: () =>
+            import('./home/home.module').then(m => m.HomeModule),
+    },
+    {
+        path: 'login',
+        loadChildren: () =>
+            import('./login/login.module').then(m => m.LoginModule),
+    },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, {
-		paramsInheritanceStrategy: 'always'
-	})],
-	exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes, {
+        paramsInheritanceStrategy: 'always'
+    })],
+    exports: [RouterModule],
 })
 export class AppRoutingModule { }
