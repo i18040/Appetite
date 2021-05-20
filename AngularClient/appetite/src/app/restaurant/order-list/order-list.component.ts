836aa3d97c1ea520fc977eec83fe0281aa3cce3c
Tobias Goetz <tobias_goetz@yahoo.de>
2021-05-20 15:33:13 +0200

    Modifications

--- AngularClient/appetite/src/app/restaurant/order-list/order-list.component.ts
+++ AngularClient/appetite/src/app/restaurant/order-list/order-list.component.ts
@@ -1,4 +1,7 @@
+import { RestaurantapiService } from './../../service/restaurantapi.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnyNsRecord } from 'node:dns';
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.scss']
 })
 export class OrderListComponent implements OnInit {
+  orderArray: any[];
 
-  constructor() { }
+  constructor(
+    private restaurantapiService: RestaurantapiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    //this.orderArray = this.restaurantapiService.fetchOrderArray();
   }
 
 }
+
+
+
+
