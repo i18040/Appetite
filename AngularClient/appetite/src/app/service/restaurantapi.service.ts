8805bf92b7e23a8360c834528c4f7b11695a859e
Tobias Goetz <tobias_goetz@yahoo.de>
2021-05-20 02:53:20 +0200

    Add restaurant pages, rudimentary stage

--- AngularClient/appetite/src/app/service/restaurantapi.service.ts
+++ AngularClient/appetite/src/app/service/restaurantapi.service.ts
@@ -1,9 +1,30 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { IGeoLocation } from '../model/geo/geoLocation';
+import { ICategory } from '../model/orderProcess/category';
+import { IRestaurantFinder } from '../model/orderProcess/restaurantFinder';
+
+import { environment as env } from 'src/environments/environment';
+import { IBodyOrder } from '../model/orderProcess/order';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantapiService {
 
-  constructor() { }
+  private token: string;
+
+  constructor(private http: HttpClient) {
+		this.token = sessionStorage.getItem('token');
+	}
+
+
+
 }
