371b6680af367ef20f352a5fffdddc054e75f823
Tobias Goetz <tobias_goetz@yahoo.de>
2021-05-20 15:42:09 +0200

    Changes

--- AngularClient/appetite/src/app/login/restaurant-register/restaurant-register.component.ts
+++ AngularClient/appetite/src/app/login/restaurant-register/restaurant-register.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
 import { Router } from '@angular/router';
@@ -89,32 +89,4 @@ export class RestaurantRegisterComponent {
 
 		return pass === confirmPass ? null : { notSame: true };
 	}
-
-	@ViewChild('fileInput') fileInput: ElementRef;
-	fileAttr = 'Choose File';
-
-
-  uploadFileEvt(imgFile: any) {
-    if (imgFile.target.files && imgFile.target.files[0]) {
-      this.fileAttr = '';
-      Array.from(imgFile.target.files).forEach((file: File) => {
-        this.fileAttr += file.name + ' - ';
-      });
-
-      // HTML5 FileReader API
-      let reader = new FileReader();
-      reader.onload = (e: any) => {
-        let image = new Image();
-        image.src = e.target.result;
-        image.onload = rs => {
-          let imgBase64Path = e.target.result;
-        };
-      };
-      reader.readAsDataURL(imgFile.target.files[0]);
-      // Reset if duplicate image uploaded again
-      this.fileInput.nativeElement.value = "";
-    } else {
-      this.fileAttr = 'Choose File';
-    }
-  }
 }
