0c4c9ee333c6bdbd6ea961706f14e8112ac08d0f
Stephan-Uhl <stephan.uhl@hansgrohe.com>
2021-05-20 02:49:40 +0200

    Add review component and WebCam

--- AngularClient/appetite/src/app/service/order.service.ts
+++ AngularClient/appetite/src/app/service/order.service.ts
@@ -8,16 +8,28 @@ import { RestServiceService } from './rest-service.service';
     providedIn: 'root'
 })
 export class OrderService {
+    private _selectedOrder: IOrder;
 
     constructor(private restService: RestServiceService) { }
+
+    public get selectedOrder(): IOrder {
+        return this._selectedOrder;
+    }
+    public set selectedOrder(value: IOrder) {
+        this._selectedOrder = value;
+    }
 
     /**
    * fetches all orders that a user placed
    * @returns IOrder[]
    */
     fetchAllOrders(): Promise<IOrder[]> {
-        const user: string = 'a@b.com';
+        const user: string = 'a@b.de';
         return this.restService.fetchOrderArray(user).toPromise();
-
     }
+
+    sendReview(text: string, rating: number, image: Object) {
+        console.log('stuff');
+    }
+
 }
