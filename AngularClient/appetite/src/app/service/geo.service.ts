10dd3c4bd870da20a33566ce162af7033f34e263
Stephan-Uhl <stephan.uhl@hansgrohe.com>
2021-06-02 21:57:49 +0200

    add comments

--- AngularClient/appetite/src/app/service/geo.service.ts
+++ AngularClient/appetite/src/app/service/geo.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
-import { IGeoLocation } from '../model/geo/geoLocation';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class GeoService {
-  constructor() {}
+    constructor() { }
 
-  async getGeoLocation(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (resp) => {
-          resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
-        },
-        (err) => {
-          throwError(err);
-          //   reject(err);
-        }
-      );
-    });
-  }
+
+    /**
+     * fetches location of the user
+     * @returns Geographical location with latitude und longitude
+     */
+    async getGeoLocation(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (resp) => {
+                    resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+                },
+                (err) => {
+                    throwError(err);
+                    //   reject(err);
+                }
+            );
+        });
+    }
 }
--- AngularClient/appetite/src/app/service/restaurantapi.service.ts
+++ AngularClient/appetite/src/app/service/restaurantapi.service.ts
@@ -4,15 +4,12 @@ import {
     HttpParams,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import { IGeoLocation } from '../model/geo/geoLocation';
-import { ICategory } from '../model/orderProcess/category';
-import { IRestaurantFinder } from '../model/orderProcess/restaurantFinder';
 
 import { environment as env } from 'src/environments/environment';
-import { IBodyOrder, IOrder } from '../model/orderProcess/order';
-import { AuthService } from './auth.service';
+import { IOrder } from '../model/orderProcess/order';
+
 
 @Injectable({
     providedIn: 'root'
